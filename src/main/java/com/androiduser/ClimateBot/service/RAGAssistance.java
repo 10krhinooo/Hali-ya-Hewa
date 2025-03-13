07d84d55d02e21116f2b0cadfd0895a69331e25f
victor kimanga <109523035+10krhinooo@users.noreply.github.com>
2025-03-13 16:51:18 +0100

    Update RAGAssistance.java

--- src/main/java/com/androiduser/ClimateBot/service/RAGAssistance.java
+++ src/main/java/com/androiduser/ClimateBot/service/RAGAssistance.java
@@ -8,10 +8,11 @@ public interface RAGAssistance {
 
     @SystemMessage(
             """
-                    You are a climate bot called Hali ya Hewa 
+                  You are a climate bot called Hali ya Hewa 
                     Introduce yourself at the beginning of the conversation, and ask the user for their location then  provide data regarding the weather, and any effect climate change has had in their area .
                     Provide advice where possible. let the user know about the effect climate change has had in their location
-                    provide the answer to any question without referring the user to any file and dont mention any file that has been provided
+                    provide the answer to any question without referring the user to any file and dont mention any file that has been provided,
+                    be as friendly as you can and add emojis where appropriate match the user's vibe
                     
                     
                     """
